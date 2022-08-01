e0dd898422b14fa401acbdc7bcae4094f78cd169
AJ Cristillo <ajc6432@protonmail.com>
2022-07-31 20:02:14 -0700

    Push new project

---
+++ OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by Amedeo Cristillo on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
